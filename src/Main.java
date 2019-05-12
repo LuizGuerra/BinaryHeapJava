cadd3e82e81053c94447687d4142017ac9c759ec
Luiz <luizpedrofg@gmail.com>
2019-05-11 23:43:34 -0300

    finalized the Binary Heap in Java :D

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // this is an example to how to use the class
+        MaxPQ<Integer> maxPQ = new MaxPQ<>(10);
+        for (int i = 0; i <  10; i ++) {
+            maxPQ.insert(i+1);
+        }
+
+        // I've maded an toString mathod, so you can check how is the things inside. :D
+        // Don't forget that the position zero in the array is null! (at least that's how my teacher teached me)
+        System.out.println(maxPQ.toString());
     }
+
 }
--- src/MaxPQ.java
+++ src/MaxPQ.java
@@ -1,45 +1,70 @@
 public class MaxPQ < Key extends Comparable<Key> > {
 
     private Key [] pq;
-    private int size;
+    private Integer n;
+    private Integer maxSize;
 
     // constructor
     public MaxPQ (int capacity) {
-        pq = (Key[]) new Comparable [capacity + 1];
-        size = 0;
+        this.pq = (Key[]) new Comparable [capacity + 1];
+        this.n = 0;
     }
 
     // PQ ops
     public Boolean isEmpty () {
-        return size == 0;
+        return this.n == 0;
     }
 
     public void insert (Key key) {
-
+        this.pq[++n] = key;
+        swim(n);
     }
 
     public Key delMax() {
-        return null;
+        Key max = this.pq[1];
+        exchange(1, n--);
+        this.pq[n] = null;
+        sink(1);
+        return max;
     }
 
-    // heap helper funcions
+    // heap helper functions
     private void swim (Integer k) {
-
+        while (k > 1 && less(k/2, k)) {
+            exchange(k, k/2);
+            k = k/2;
+        }
     }
 
     private void sink (Integer k) {
-
+        while (k*2 <= n) {
+            int j = 2*k;
+            if ( j < n && less(j, j+1)) { j++; }
+            if ( !less(k, j) ) { break; }
+            exchange(k, j);
+            k = j;
+        }
     }
 
     // array helper functions
     private boolean less (Integer i, Integer j) {
-        return false;
+        return this.pq[i].compareTo(this.pq[j]) < 0;
     }
 
-    private void exchange (Integer i, Integer k) {
-
+    private void exchange (Integer i, Integer j) {
+        Key key = this.pq[i];
+        this.pq[i] = this.pq[j];
+        this.pq[j] = key;
     }
 
-
+    @Override
+    public String toString () {
+        String toString = "";
+        for (int i = 0; i <= this.n; i++) {
+            toString += "Position " + i + ": " + this.pq[i];
+            if (i != this.n ) { toString += "\n"; }
+        }
+        return toString;
+    }
 
 }
